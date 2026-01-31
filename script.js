6cb1ad3fd91c2328b34b4b857ff9f81a855517b7
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-01-31 14:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let sArray=str.split("");
+for (let i of str) {
+    let f = sArray.filter(s => s===i);
+    if (f.length===1) return i;
 }
+
+return null;
+}
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
